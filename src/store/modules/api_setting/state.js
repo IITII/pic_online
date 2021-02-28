f34a3ffae12ceaf4fa98be2147e464fe6a4eb40a
IITII <ccmejx@gmail.com>
2021-02-28 23:46:13 +0800

    feature: Using site domain as the default api domain

--- src/store/modules/api_setting/state.js
+++ src/store/modules/api_setting/state.js
@@ -1,14 +1,21 @@
 import dataPersistent from 'src/utils/persistent'
 
+const apiPort = 3000
+const apiBasic = `${window.location.protocol}//${window.location.hostname}:${apiPort}`
+const apiPrefix = {
+  treeUrl: '/private/tree',
+  picUrl: '/private/pic'
+}
+
 const state = {
   // tree api 地址
   treeUrl: dataPersistent.apiGetItem('treeUrl') ||
-    process.env.VUE_APP_TREE_API_URL || 'http://localhost:3000/private/tree',
+    process.env.VUE_APP_TREE_API_URL || `${apiBasic}${apiPrefix.treeUrl}`,
   // tree api 请求方法
   treeMethod: dataPersistent.apiGetItem('treeMethod') || 'GET',
   // pic api 地址
   picUrl: dataPersistent.apiGetItem('picUrl') ||
-    process.env.VUE_APP_PIC_API_URL || 'http://localhost:3000/private/pic',
+    process.env.VUE_APP_PIC_API_URL || `${apiBasic}${apiPrefix.picUrl}`,
   // pic api 请求方法
   picMethod: dataPersistent.apiGetItem('picMethod') || 'POST'
 }
