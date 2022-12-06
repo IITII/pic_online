11ed7431ab60450169c74e9d3e7d1164d54cf056
IITII <ccmejx@gmail.com>
2022-12-06 23:47:54 +0800

    feature: log sum

--- src/components/commons/PicReloadFiles.vue
+++ src/components/commons/PicReloadFiles.vue
@@ -26,10 +26,17 @@ export default {
   },
   methods: {
     reload() {
+      const start = Date.now()
       this.loading = true
       return this.$axios.post('/api/file/reload', {})
-        .then(() => {
-          this.$q.notify({type: 'info', message: this.$t('success')})
+        .then(sum => {
+          const end = ((Date.now() - start) / 1000).toFixed(2)
+          this.$log.debug('/api/file/reload', sum)
+          this.$q.notify({
+            type: 'info',
+            message: this.$t('success'),
+            caption: `图片总数: ${sum?.imgCount}, 视频总数: ${sum?.videoCount}, cost: ${end}s`,
+          })
         })
         .finally(() => {
           this.loading = false
