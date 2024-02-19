8d9533da64bf9e41f2d0479c51cca9255259af84
IITII <ccmejx@gmail.com>
2024-02-19 13:12:51 +0800

    wip: emby

---
+++ src/pages/admin/PicEmby.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="q-pt-md">
+    <moleculer-status/>
+  </div>
+</template>
+
+<script>
+/**
+ * @author IITII <ccmejx@gmail.com>
+ * @date 2021/06/03 10:42
+ */
+'use strict'
+import MoleculerStatus from 'components/moleculer_status'
+
+export default {
+  name: 'PicEmby',
+  components: {MoleculerStatus}
+}
+</script>
+
+<style scoped>
+
+</style>
